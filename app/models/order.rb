f6e0eff459af1656992eb98829f6cae197fa8412
Michal Valasek <michal.valasek@gmail.com>
2011-11-25 16:05:50 +0100

    Finished chapter 12, added orders, infochannels, pagination

---
+++ app/models/order.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+class Order < ActiveRecord::Base
+
+	PAYMENT_TYPES = ["Prevod", "Platobná karta", "Dobierka"]
+
+	has_many :line_items, :dependent => :destroy
+
+  validates :name, :address, :email, :presence => true
+  validates :pay_type, :inclusion => PAYMENT_TYPES
+
+	def add_line_items_from_cart(cart)
+		cart.line_items.each do |li|
+			li.cart_id = nil
+			line_items << li
+		end
+	end
+
+end
